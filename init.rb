7cd8adfe844b73581bc830683e6b51a3e0f38645
ryuthky knfs <ryuayu.rose@gmail.com>
2015-12-02 17:13:50 +0900

    project menu tab

--- init.rb
+++ init.rb
@@ -10,8 +10,12 @@ Redmine::Plugin.register :redmine_chart do
     permission :view_chart,  :redmine_chart =>[:index, :show]
     permission :manage_chart, :redmine_chart =>[:new, :edit, :create, :update, :destroy, :preview], :require => :member
   end
-  
-  permission :redmine_chart,{:redmine_chart => [:index]}, public =>true
-  menu :project_menu, :redmine_chart, {:controller =>'redmine_chart', :action =>'index'}, :param => :project_id, :caption => 'charts', :first =>true,
+ 
+  permission :redmine_chart, { :redmine_chart => [:index] }, :public => true
+  menu :project_menu, :test , {:controller =>'redmine_chart', :action =>'index'}, :param => :project_id, :caption => 'charts', :after => 'gantt'
 
+# プロジェクトごとの権限を追加
+ project_module :test do
+    permission :view_sample, :sample => :index
+ end
 end
