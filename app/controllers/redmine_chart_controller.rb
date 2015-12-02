6754e9a2f81413ce49fc4ec5c0eb5426f4008aa0
ryuthky knfs <ryuayu.rose@gmail.com>
2015-12-02 16:38:00 +0900

    find_id add

--- app/controllers/redmine_chart_controller.rb
+++ app/controllers/redmine_chart_controller.rb
@@ -2,6 +2,7 @@ class RedmineChartController < ApplicationController
   unloadable
   menu_item :redmine_chart
   brfore_filter :find_project, :authorize
+  before_filter :find_redmine_chart, :except => [:index, :new, :create, :preview]
   # :
   def index
     @redmin_chart =Redmine_chart.find(:all, :conditions => ["project_id = #{@project.id} "])
@@ -15,6 +16,11 @@ class RedmineChartController < ApplicationController
 
   def edit
   end
+  
+  def preview
+    @text = params[:redmine_chart][:description]
+    render :partial => 'common/preview'
+  end
 private
   def find_project
   #project = param[:project_id] 
@@ -22,5 +28,9 @@ private
       @redmine_chart =Redmine_chart.find(:all)
   rescue ActiveRecoed::RecordNotFound
       render_404
+  end
+  def find_redmine_chart
+     @redmine_chart = Redmine_chart.find_by_id(params[:id]);
+     render_404 unless @redmine_chart
   end
 end
