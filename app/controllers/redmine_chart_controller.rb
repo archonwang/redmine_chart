7fff759d09296b54755f361d3598c83379a33ff6
ryuthky knfs <ryuayu.rose@gmail.com>
2015-12-09 13:30:39 +0900

    test chart 3 add

--- app/controllers/redmine_chart_controller.rb
+++ app/controllers/redmine_chart_controller.rb
@@ -18,7 +18,7 @@ class RedmineChartController < ApplicationController
         ['hage', 0]
       ]
     })
-   end
+    end
     category = [1,3,5,7]
     current_quantity = [1000,5000,3000,8000]
 
@@ -26,6 +26,19 @@ class RedmineChartController < ApplicationController
       f.title(text: 'ItemXXXの在庫の推移')
       f.xAxis(categories: category)
       f.series(name: '在庫数', data: current_quantity)
+    end
+	
+	@multiple = LazyHighCharts::HighChart.new('graph') do |f|
+        f.title(:text => "Population vs GDP For 5 Big Countries [2009]")
+        f.xAxis(:categories => ["United States", "Japan", "China", "Germany", "France"])
+        f.series(:name => "GDP in Billions", :yAxis => 0, :data => [14119, 5068, 4985, 3339, 2656])
+        f.series(:name => "Population in Millions", :yAxis => 1, :data => [310, 127, 1340, 81, 65])
+        f.yAxis [
+          {:title => {:text => "GDP in Billions", :margin => 70} },
+          {:title => {:text => "Population in Millions"}, :opposite => true},
+        ]
+        f.legend(:align => 'right', :verticalAlign => 'top', :y => 75, :x => -50, :layout => 'vertical',)
+        f.chart({:defaultSeriesType=>"column"})
     end
   end
 
