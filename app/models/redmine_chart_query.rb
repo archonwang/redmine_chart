47034187827f92c1857b61dddbfea6bebc578849
ryuthky knfs <ryuayu.rose@gmail.com>
2016-01-26 12:52:04 +0900

    filter simple

--- app/models/redmine_chart_query.rb
+++ app/models/redmine_chart_query.rb
@@ -4,5 +4,67 @@ class RedmineChartQuery < IssueQuery
     super attributes
     self.filters.delete('status_id')
   end
+ def initialize_available_filters
+    principals = []
+    subprojects = []
+    versions = []
+    categories = []
+    issue_custom_fields = []
 
+    if project
+      principals += project.principals.visible
+      unless project.leaf?
+        subprojects = project.descendants.visible.to_a
+        principals += Principal.member_of(subprojects).visible
+      end
+      versions = project.shared_versions.to_a
+      categories = project.issue_categories.to_a
+      issue_custom_fields = project.all_issue_custom_fields
+    else
+      if all_projects.any?
+        principals += Principal.member_of(all_projects).visible
+      end
+      versions = Version.visible.where(:sharing => 'system').to_a
+      issue_custom_fields = IssueCustomField.where(:is_for_all => true)
+    end
+    principals.uniq!
+    principals.sort!
+    principals.reject! {|p| p.is_a?(GroupBuiltin)}
+    users = principals.select {|p| p.is_a?(User)}
+
+    if project.nil?
+      project_values = []
+      if User.current.logged? && User.current.memberships.any?
+        project_values << ["<< #{l(:label_my_projects).downcase} >>", "mine"]
+      end
+      project_values += all_projects_values
+      add_available_filter("project_id",
+        :type => :list, :values => project_values
+      ) unless project_values.empty?
+    end
+
+
+    assigned_to_values = []
+    assigned_to_values << ["<< #{l(:label_me)} >>", "me"] if User.current.logged?
+    assigned_to_values += (Setting.issue_group_assignment? ?
+                              principals : users).collect{|s| [s.name, s.id.to_s] }
+    add_available_filter("assigned_to_id",
+      :type => :list_optional, :values => assigned_to_values
+    ) unless assigned_to_values.empty?
+
+
+    if versions.any?
+      add_available_filter "fixed_version_id",
+        :type => :list_optional,
+        :values => versions.sort.collect{|s| ["#{s.project.name} - #{s.name}", s.id.to_s] }
+    end
+
+    if categories.any?
+      add_available_filter "category_id",
+        :type => :list_optional,
+        :values => categories.collect{|s| [s.name, s.id.to_s] }
+    end
+
+
+ end
 end
