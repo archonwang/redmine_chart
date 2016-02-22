f7cf7e9f01f973d39d05dc568e74a03ffe08cf49
Mori Ayuko for Win <mori_ayuk@example.com>
2016-02-22 14:10:09 +0900

    Rel_1_0 debug22

--- app/controllers/redmine_chart_controller.rb
+++ app/controllers/redmine_chart_controller.rb
@@ -172,7 +172,7 @@ logger.debug("====================<")
         @before_date_by_count = 0
         @before_date_close_count = 0
         # range issues start count 
-        @range_issues = @issues.select{| hash | hash[:start_date] >= @enable_start_date and hash[:start_date]<= @all_last_date }
+        @range_issues = @issues.select{| hash | hash[:start_date].nil? == true and hash[:start_date] >= @enable_start_date and hash[:start_date]<= @all_last_date }
 logger.debug(">======")
 
         # 描画開始前該当チケット抽出
