984b9caf0828908be6f5162b60054c8f953bfde3
Mori Ayuko for Win <mori_ayuk@example.com>
2016-02-22 14:05:44 +0900

    Rel_1_0 debug21

--- app/controllers/redmine_chart_controller.rb
+++ app/controllers/redmine_chart_controller.rb
@@ -162,11 +162,7 @@ logger.debug("====================<")
 logger.debug(">====================all_date")
 logger.debug( @all_last_date)
 logger.debug( @all_first_date)
-
-logger.debug( @enable_start_date)
-             if @enable_start_date.nil? then
                @enable_start_date = @project_start_date
-             endif
 logger.debug( @enable_start_date)
 
 logger.debug("====================<")
